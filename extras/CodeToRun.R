77dd6225fe0bb843e703605ba76077ecb743a7f0
edward-burn <9583964+edward-burn@users.noreply.github.com>
2021-03-17 09:47:56 +0100

    updates

--- extras/CodeToRun.R
+++ extras/CodeToRun.R
@@ -1,32 +1,33 @@
+
+#install.packages("renv") # if not already installed, install renv from CRAN
+renv::restore() # this should prompt you to install the various packages required for the study
+
+# study package (you should be inside the project, and have built the package)
 library(PLEMSKAIalloutcomes)
 
 # Optional: specify where the temporary files (used by the Andromeda package) will be created:
 options(andromedaTempFolder = "s:/andromedaTemp")
 
 # Maximum number of cores to be used:
-maxCores <- parallel::detectCores()
+maxCores <-parallel::detectCores()
 
 # The folder where the study intermediate and result files will be written:
-outputFolder <- "s:/PLEMSKAIalloutcomes"
+outputFolder <- "....."
 
 # Details for connecting to the server:
-connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "pdw",
-                                                                server = Sys.getenv("PDW_SERVER"),
-                                                                user = NULL,
-                                                                password = NULL,
-                                                                port = Sys.getenv("PDW_PORT"))
+connectionDetails <- DatabaseConnector::createConnectionDetails(".....")
 
 # The name of the database schema where the CDM data can be found:
-cdmDatabaseSchema <- "CDM_IBM_MDCD_V1153.dbo"
+cdmDatabaseSchema <- "....."
 
 # The name of the database schema and table where the study-specific cohorts will be instantiated:
-cohortDatabaseSchema <- "scratch.dbo"
-cohortTable <- "mschuemi_skeleton"
+cohortDatabaseSchema <- "....."
+cohortTable <- "....."
 
 # Some meta-information that will be used by the export function:
-databaseId <- "Synpuf"
-databaseName <- "Medicare Claims Synthetic Public Use Files (SynPUFs)"
-databaseDescription <- "Medicare Claims Synthetic Public Use Files (SynPUFs) were created to allow interested parties to gain familiarity using Medicare claims data while protecting beneficiary privacy. These files are intended to promote development of software and applications that utilize files in this format, train researchers on the use and complexities of Centers for Medicare and Medicaid Services (CMS) claims, and support safe data mining innovations. The SynPUFs were created by combining randomized information from multiple unique beneficiaries and changing variable values. This randomization and combining of beneficiary information ensures privacy of health information."
+databaseId <- "....."
+databaseName <- "....."
+databaseDescription <- "....."
 
 # For Oracle: define a schema that can be used to emulate temp tables:
 oracleTempSchema <- NULL
